0ce6bd2409a14d70ef0e73db8f44ee7bc354ebd3
arik-dig <asher@digma.ai>
2022-06-19 16:20:24 +0300

    add grpc-hello-world

---
+++ src/grpc-helloworld/client/main.go
@@ -0,0 +1,215 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"time"
+
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+
+	"github.com/digma-ai/otel-sample-application-go/grpc-helloworld/api"
+	"github.com/digma-ai/otel-sample-application-go/grpc-helloworld/config"
+	"github.com/digma-ai/otel-sample-application-go/src/otelconfigure"
+)
+
+func main() {
+	tp, err := config.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+
+	shutdownTracer := otelconfigure.InitTracerWithModule("helloworld-client",
+		"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc/example",
+		"C:\\Users\\arik\\Documents\\GitHub\\opentelemetry-go-contrib\\instrumentation\\google.golang.org\\grpc\\otelgrpc\\example",
+		[]string{
+			"github.com/digma-ai/otel-sample-application-go/src/otelconfigure",
+		})
+	defer shutdownTracer()
+
+	var conn *grpc.ClientConn
+	conn, err = grpc.Dial(":7777", grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor()),
+		grpc.WithStreamInterceptor(otelgrpc.StreamClientInterceptor()),
+	)
+
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer func() { _ = conn.Close() }()
+
+	c := api.NewHelloServiceClient(conn)
+
+	if err := callSayHello(c); err != nil {
+		log.Fatal(err)
+	}
+	if err := callSayHelloClientStream(c); err != nil {
+		log.Fatal(err)
+	}
+	if err := callSayHelloServerStream(c); err != nil {
+		log.Fatal(err)
+	}
+	if err := callSayHelloBidiStream(c); err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(10 * time.Millisecond)
+}
+
+func callSayHello(c api.HelloServiceClient) error {
+	md := metadata.Pairs(
+		"timestamp", time.Now().Format(time.StampNano),
+		"client-id", "web-api-client-us-east-1",
+		"user-id", "some-test-user-id",
+	)
+
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	response, err := c.SayHello(ctx, &api.HelloRequest{Greeting: "World"})
+	if err != nil {
+		return fmt.Errorf("calling SayHello: %w", err)
+	}
+	log.Printf("Response from server: %s", response.Reply)
+	return nil
+}
+
+func callSayHelloClientStream(c api.HelloServiceClient) error {
+	md := metadata.Pairs(
+		"timestamp", time.Now().Format(time.StampNano),
+		"client-id", "web-api-client-us-east-1",
+		"user-id", "some-test-user-id",
+	)
+
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	stream, err := c.SayHelloClientStream(ctx)
+	if err != nil {
+		return fmt.Errorf("opening SayHelloClientStream: %w", err)
+	}
+
+	for i := 0; i < 5; i++ {
+		err := stream.Send(&api.HelloRequest{Greeting: "World"})
+
+		time.Sleep(time.Duration(i*50) * time.Millisecond)
+
+		if err != nil {
+			return fmt.Errorf("sending to SayHelloClientStream: %w", err)
+		}
+	}
+
+	response, err := stream.CloseAndRecv()
+	if err != nil {
+		return fmt.Errorf("closing SayHelloClientStream: %w", err)
+	}
+
+	log.Printf("Response from server: %s", response.Reply)
+	return nil
+}
+
+func callSayHelloServerStream(c api.HelloServiceClient) error {
+	md := metadata.Pairs(
+		"timestamp", time.Now().Format(time.StampNano),
+		"client-id", "web-api-client-us-east-1",
+		"user-id", "some-test-user-id",
+	)
+
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	stream, err := c.SayHelloServerStream(ctx, &api.HelloRequest{Greeting: "World"})
+	if err != nil {
+		return fmt.Errorf("opening SayHelloServerStream: %w", err)
+	}
+
+	for {
+		response, err := stream.Recv()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return fmt.Errorf("receiving from SayHelloServerStream: %w", err)
+		}
+
+		log.Printf("Response from server: %s", response.Reply)
+		time.Sleep(50 * time.Millisecond)
+	}
+	return nil
+}
+
+func callSayHelloBidiStream(c api.HelloServiceClient) error {
+	md := metadata.Pairs(
+		"timestamp", time.Now().Format(time.StampNano),
+		"client-id", "web-api-client-us-east-1",
+		"user-id", "some-test-user-id",
+	)
+
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	stream, err := c.SayHelloBidiStream(ctx)
+	if err != nil {
+		return fmt.Errorf("opening SayHelloBidiStream: %w", err)
+	}
+
+	serverClosed := make(chan struct{})
+	clientClosed := make(chan struct{})
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			err := stream.Send(&api.HelloRequest{Greeting: "World"})
+
+			if err != nil {
+				// nolint: revive  // This acts as its own main func.
+				log.Fatalf("Error when sending to SayHelloBidiStream: %s", err)
+			}
+
+			time.Sleep(50 * time.Millisecond)
+		}
+
+		err := stream.CloseSend()
+		if err != nil {
+			// nolint: revive  // This acts as its own main func.
+			log.Fatalf("Error when closing SayHelloBidiStream: %s", err)
+		}
+
+		clientClosed <- struct{}{}
+	}()
+
+	go func() {
+		for {
+			response, err := stream.Recv()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				// nolint: revive  // This acts as its own main func.
+				log.Fatalf("Error when receiving from SayHelloBidiStream: %s", err)
+			}
+
+			log.Printf("Response from server: %s", response.Reply)
+			time.Sleep(50 * time.Millisecond)
+		}
+
+		serverClosed <- struct{}{}
+	}()
+
+	// Wait until client and server both closed the connection.
+	<-clientClosed
+	<-serverClosed
+	return nil
+}
---
+++ src/grpc-helloworld/server/main.go
@@ -0,0 +1,169 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+
+	"github.com/digma-ai/otel-sample-application-go/grpc-helloworld/api"
+	"github.com/digma-ai/otel-sample-application-go/grpc-helloworld/config"
+	"github.com/digma-ai/otel-sample-application-go/src/otelconfigure"
+
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+
+	digmagrpc "github.com/digma-ai/otel-go-instrumentation/grpc"
+)
+
+const (
+	port = ":7777"
+)
+
+var tracer = otel.Tracer("grpc-helloworld")
+
+// server is used to implement api.HelloServiceServer.
+type server struct {
+	api.HelloServiceServer
+}
+
+// SayHello implements api.HelloServiceServer.
+func (s *server) SayHello(ctx context.Context, in *api.HelloRequest) (*api.HelloResponse, error) {
+	log.Printf("Received: %v\n", in.GetGreeting())
+	s.workHard(ctx)
+	time.Sleep(50 * time.Millisecond)
+
+	return &api.HelloResponse{Reply: "Hello " + in.Greeting}, nil
+}
+
+func (s *server) workHard(ctx context.Context) {
+	_, span := tracer.Start(ctx, "workHard",
+		trace.WithAttributes(attribute.String("extra.key", "extra.value")))
+	defer span.End()
+
+	time.Sleep(50 * time.Millisecond)
+}
+
+func (s *server) SayHelloServerStream(in *api.HelloRequest, out api.HelloService_SayHelloServerStreamServer) error {
+	log.Printf("Received: %v\n", in.GetGreeting())
+
+	for i := 0; i < 5; i++ {
+		err := out.Send(&api.HelloResponse{Reply: "Hello " + in.Greeting})
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(time.Duration(i*50) * time.Millisecond)
+	}
+
+	return nil
+}
+
+func (s *server) SayHelloClientStream(stream api.HelloService_SayHelloClientStreamServer) error {
+	i := 0
+
+	for {
+		in, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Printf("Non EOF error: %v\n", err)
+			return err
+		}
+
+		log.Printf("Received: %v\n", in.GetGreeting())
+		i++
+	}
+
+	time.Sleep(50 * time.Millisecond)
+
+	return stream.SendAndClose(&api.HelloResponse{Reply: fmt.Sprintf("Hello (%v times)", i)})
+}
+
+func (s *server) SayHelloBidiStream(stream api.HelloService_SayHelloBidiStreamServer) error {
+	for {
+		in, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Printf("Non EOF error: %v\n", err)
+			return err
+		}
+
+		time.Sleep(50 * time.Millisecond)
+
+		log.Printf("Received: %v\n", in.GetGreeting())
+		err = stream.Send(&api.HelloResponse{Reply: "Hello " + in.Greeting})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	tp, err := config.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	shutdown := otelconfigure.InitTracerWithModule("helloworld-server",
+		"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc/example",
+		"C:\\Users\\arik\\Documents\\GitHub\\opentelemetry-go-contrib\\instrumentation\\google.golang.org\\grpc\\otelgrpc\\example",
+		[]string{
+			"github.com/digma-ai/otel-sample-application-go/src/otelconfigure",
+		})
+	defer shutdown()
+
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			otelgrpc.UnaryServerInterceptor(),
+			digmagrpc.UnaryServerInterceptor(),
+		)),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			otelgrpc.StreamServerInterceptor(),
+			digmagrpc.StreamServerInterceptor(),
+		)),
+	)
+
+	api.RegisterHelloServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
